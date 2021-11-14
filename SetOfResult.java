b53e72a10e711105368daac5aa9a58f880de9deb
Rahafq07 <ralsaeed0010@stu.kau.edu.sa>
2021-11-14 19:38:53 +0300

    SetOfResult.java updated

---
+++ SetOfResult.java
@@ -0,0 +1,16 @@
+
+/**
+ * <h1>SetOfResult Class</h1>
+ * This is the SetOfResult class which has parent and weight as instance variable.
+ * 
+ * @author Rahaf , sarah , Somayah 
+ * @version IDE 8.2
+ * @since 11-11-2021
+ */
+
+public class SetOfResult {
+
+    int parent;
+    int weight;
+}
+
