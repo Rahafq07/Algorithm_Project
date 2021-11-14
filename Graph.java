6e6faf60282ec0a0680d7f1cc662db32c931305a
Rahafq07 <ralsaeed0010@stu.kau.edu.sa>
2021-11-14 19:43:19 +0300

    Graph.java updated

--- Graph.java
+++ Graph.java
@@ -75,3 +75,30 @@ public class Graph {
     }
 
 }
+
+/***
+ * <h1>Edge Class</h1>
+ * This is the Graph class which has source,destination and weight as instance variable.
+ * 
+ * @author Rahaf , sarah , Somayah 
+ */
+class Edge { 
+
+    int source; // integer number represent the source
+    int destination; // integer number represent the destination
+    int weight; // the weight of edge
+
+    /***
+     * This is a Edge constructor that indicates parameters
+     * 
+     * @param source
+     * @param destination
+     * @param weight 
+     */
+    public Edge(int source, int destination, int weight) { // Edge contractor to add new Edge
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+}
+
