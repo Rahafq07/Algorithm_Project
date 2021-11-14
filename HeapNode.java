ad06ac9f3776cc9c21f32a9bcf619063cd7f33ef
Rahafq07 <ralsaeed0010@stu.kau.edu.sa>
2021-11-14 19:38:07 +0300

    HeapNode.java updated

---
+++ HeapNode.java
@@ -0,0 +1,16 @@
+
+/**
+ * <h1>HeapNode Class</h1>
+ * This is the HeapNode class which has vertex and key as instance variable.
+ * 
+ * @author Rahaf , sarah , Somayah 
+ * @version IDE 8.2
+ * @since 11-11-2021
+ */
+
+public class HeapNode {
+
+    int vertex;
+    int key;
+}
+
